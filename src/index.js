29f84a30e04dc23b09ea86ff5edfe089f34943d9
David Lafontant <lafontant.david@gmail.com>
2024-03-09 15:57:21 -0500

    Setup index,js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
+import _ from 'lodash';
+import printMe from './print.js';
+import './styles/style.css';
+import picture from './images/flower.jpg';
+
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add("hello");
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  const flower = new Image();
+  flower.src = picture;
+  flower.classList.add("flower");
+
+
+  element.appendChild(flower);
+  element.appendChild(btn);
 
   return element;
 }
 
 document.body.appendChild(component());
+
+
+if (module.hot) {
+
+  module.hot.accept('./print.js', function () {
+
+    console.log('Accepting the updated printMe module!');
+
+    printMe();
+
+  })
+
+}
